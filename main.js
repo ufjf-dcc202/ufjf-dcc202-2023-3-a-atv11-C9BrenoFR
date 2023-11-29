8cdf34a3d89ade1bd5e73a85a1338d9f2f643ce6
Breno Furtado Rosado <breno.rosado@estudante.ufjf.br>
2023-11-29 20:05:57 -0300

    Cria o arquivo main,js

---
+++ main.js
@@ -0,0 +1,13 @@
+// main.js
+import {getLista} from getLista.js;
+
+const pEntrada = document.querySelector('#entrada');
+const olSaida = document.querySelector('#saida');
+const btnAdicionar = document.querySelector('#adicionar');
+const btnLimpar = document.querySelector('#limpar');
+
+olSaida.innerHTML = "";
+
+function atualizar(params) {
+    
+}
